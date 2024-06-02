0c82f95b50673732342e3755c2ec8f3b6cbfed30
Atanas Stoev <NFSpeedy@users.noreply.github.com>
2024-06-03 00:14:10 +0300

    chore: Add basic Tkinter window functionality

---
+++ src/tkinter_tutorial/01_basic_window.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+
+root = tk.Tk()  # Main window instance
+root.title("Basic Window")  # Title of the window
+root.geometry("400x300")  # Size of the window
+
+if __name__ == '__main__':
+    root.mainloop()  # Start the GUI event loop
